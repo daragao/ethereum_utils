3b0a5354e55ecfd7cce600500ca9fa2eafb7f23e
Duarte Aragao <daragao@gmail.com>
2018-07-03 17:54:27 +0100

    example of creating txhash with ethereum libs

--- random_connection.go
+++ random_connection.go
@@ -4,12 +4,51 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"math/rand"
+	"time"
 
-	_ "github.com/ethereum/go-ethereum/common/hexutil"
+	// "github.com/ethereum/go-ethereum/crypto"
+	// "github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/ethdb"
 	"github.com/ethereum/go-ethereum/rlp"
+	"github.com/ethereum/go-ethereum/trie"
 )
+
+func random(min, max int) int {
+	rand.Seed(time.Now().Unix())
+	return rand.Intn(max-min) + min
+}
+
+func GetTxTrie(block *types.Block) *trie.Trie {
+	trieDB := trie.NewDatabase(ethdb.NewMemDatabase())
+	trieObj, _ := trie.New(common.Hash{}, trieDB) // empty trie
+	for idx, tx := range block.Transactions() {
+
+		rlpIdx, _ := rlp.EncodeToBytes(uint(idx))  // rlp encode index of transaction
+		rlpTransaction, _ := rlp.EncodeToBytes(tx) // rlp encode transaction
+
+		trieObj.Update(rlpIdx, rlpTransaction) // update trie with the rlp encode index and the rlp encoded transaction
+		root, err := trieObj.Commit(nil)       // commit to database (which in this case is stored in memory)
+		if err != nil {
+			log.Fatalf("commit error: %v", err)
+		}
+
+		txRlpHash := crypto.Keccak256Hash(rlpTransaction)
+
+		fmt.Printf("TxHash[%d]: \t% 0x\n\tHash(RLP(Tx)): \t% 0x\n\tTrieRoot: \t% 0x\n",
+			idx, tx.Hash().Bytes(), txRlpHash.Bytes(), root.Bytes())
+		//fmt.Printf("\n%#v\n% #v\n% 0x\n\n", trieObj, trieObj, root)
+	}
+
+	fmt.Printf("\n\nBlock number: %d \n\tBlock.TxHash:\t% 0x \n\tTrie.Root:\t% 0x\n",
+		block.Number, block.TxHash().Bytes(), trieObj.Root())
+
+	return trieObj
+}
 
 func main() {
 	client, err := ethclient.Dial("https://mainnet.infura.io")
@@ -19,6 +58,8 @@ func main() {
 
 	fmt.Println("we have a connection")
 	//_ = client // we'll use this in the upcoming sections
+
+	// blockNumber := big.NewInt(7295200)
 
 	header, err := client.HeaderByNumber(context.Background(), nil)
 	if err != nil {
@@ -30,18 +71,23 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println(header.Number.String()) // 5671744
+	/*
+		fmt.Println(header.Number.String()) // 5671744
 
-	count, err := client.TransactionCount(context.Background(), block.Hash())
-	if err != nil {
-		log.Fatal(err)
-	}
+		count, err := client.TransactionCount(context.Background(), block.Hash())
+		if err != nil {
+			log.Fatal(err)
+		}
+	*/
 
-	fmt.Println(count)
-	fmt.Printf("\n%+v\n", header)
-	rlpHeader, _ := rlp.EncodeToBytes(&header)
-	headerHash := crypto.Keccak256Hash(rlpHeader)
-	fmt.Printf("\n%x == %x\n", headerHash, block.Hash())
+	/*
+		// hash header
+		fmt.Println(count)
+		fmt.Printf("\n%+v\n", header)
+		rlpHeader, _ := rlp.EncodeToBytes(&header)
+		headerHash := crypto.Keccak256Hash(rlpHeader)
+		fmt.Printf("\n%x == %x\n", headerHash, block.Hash())
+	*/
 
 	/*
 			fmt.Println("==== // ===")
@@ -57,7 +103,17 @@ func main() {
 		}
 	*/
 
-	data := []byte("hello")
-	hash := crypto.Keccak256Hash(data)
-	fmt.Println("\"hello\" keccak hash:\t", hash.Hex())
+	trieObj := GetTxTrie(block)
+	txIdx := uint(random(0, len(block.Transactions())))
+	rlpIdx, _ := rlp.EncodeToBytes(txIdx)
+	txRlpBytes := trieObj.Get(rlpIdx)
+	txRlpHash := crypto.Keccak256Hash(txRlpBytes).Bytes()
+
+	fmt.Printf("Retrieval from Trie == Block TxHash\n\tHash(Trie.Get(%d)): \t% 0x\n\tBlock.TxHash[%d]: \t% 0x\n",
+		txIdx, txRlpHash, txIdx, block.Transactions()[txIdx].Hash().Bytes())
+	/*
+		data := []byte("hello")
+		hash := crypto.Keccak256Hash(data)
+		fmt.Println("\"hello\" keccak hash:\t", hash.Hex())
+	*/
 }
